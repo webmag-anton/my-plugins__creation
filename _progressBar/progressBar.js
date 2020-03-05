912d85474aca8b930996d411126d3b1a6ea0a5af
Anton <lyooon92@gmail.com>
2020-03-05 13:56:51 +0200

    1

--- _progressBar/progressBar.js
+++ _progressBar/progressBar.js
@@ -22,21 +22,24 @@ class ProgressBar {
 		this.border = border
 	}
 
-	init(selector) { // метод init добавляет внутрь выбранного элемента progressBar из метода createProgressBar  -1-
-		document.querySelector(selector).append(this.createProgressBar())
+	// init - единственный публичный метод, о котором должен знать пользователь 
+	// защищенные методы, начинающиеся с _ являются инкапсулироваными: о них пользователь не должен знать
+
+	init(selector) { // метод init добавляет внутрь выбранного элемента progressBar из метода _createProgressBar  -1-
+		document.querySelector(selector).append( this._createProgressBar() )
 	}
 
-	createProgressBar() { // метод createProgressBar создает progressBar внутрь которого добавляет bar из метода createBar  -2-
+	_createProgressBar() { // метод _createProgressBar создает progressBar внутрь которого добавляет bar из метода _createBar  -2-
 		const progressBar = document.createElement('div')
-		const bar = this.createBar()
+		const bar = this._createBar()
 		progressBar.style.width = '100%'
 		progressBar.style.border = this.border
 		progressBar.append(bar)
-		this.animateBar(bar) // и анимирует этот внутренний бар методом animateBar
+		this._animateBar(bar) // и анимирует этот внутренний бар методом _animateBar
 		return progressBar
 	}
 
-	createBar() { // метод createBar создает bar  -3-
+	_createBar() { // метод _createBar создает bar  -3-
 		const bar = document.createElement('div')
 		bar.style.cssText = `
 			text-align: center;
@@ -45,20 +48,20 @@ class ProgressBar {
 			line-height: ${this.height}px;
 			color: ${this.textColor};
 		`
-		this.stateProgress(bar) // и задает ширину bar и проценты внутри bar методом stateProgress
+		this._stateProgress(bar) // и задает ширину bar и проценты внутри bar методом _stateProgress
 		return bar
 	}
 
-	stateProgress(elem) {  //  -4- , 					-6, 7, 8... (вызов из рекурсии пока выполняется условие в анимации)-
+	_stateProgress(elem) {  //  -4- , 					-6, 7, 8... (вызов из рекурсии пока выполняется условие в анимации)-
 		elem.style.width = `${this.start}%`
 		elem.textContent = `${this.start}%`
 	}
 
-	animateBar(bar) { // метод для анимации bar  -5-
+	_animateBar(bar) { // метод для анимации bar  -5-
 		const animate = () => {
 			if (this.start < this.end) {
 				this.start++;
-				this.stateProgress(bar)
+				this._stateProgress(bar)
 				requestAnimationFrame(animate)
 			}
 		}
@@ -77,14 +80,14 @@ class RoundedProgressBar extends ProgressBar {
 		this.rounded = options.rounded || '5px'
 	}
 
-	// изменяем (расширяем) метод createProgressBar - пример полиморфизма (изменяем одноименный родительский метод)
-	createProgressBar() {
-		const progressBar = super.createProgressBar() // вызов родительского методв
-		this.roundedBar(progressBar)
+	// изменяем (расширяем) метод _createProgressBar - пример полиморфизма (изменяем одноименный родительский метод)
+	_createProgressBar() {
+		const progressBar = super._createProgressBar() // вызов родительского методв
+		this._roundedBar(progressBar)
 		return progressBar
 	}
 
-	roundedBar(elem) {
+	_roundedBar(elem) {
 		elem.style.borderRadius = this.rounded
 		elem.style.overflow = 'hidden'
 	}
